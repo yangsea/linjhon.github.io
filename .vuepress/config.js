c0e51dfa9b835045e1de14c88f3b49eb85a9bd85
linjhon <linjhon@foxmail.com>
2021-04-14 16:49:58 +0800

    feat: pwa 配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -28,5 +28,30 @@ module.exports = {
         content: "width=device-width,initial-scale=1,user-scalable=no",
       },
     ],
+    ["link", { rel: "icon", href: "/img/maple_leaf.png" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/img/maple_leaf.png" }],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/img/maple_leaf.png",
+        color: "#3eaf7c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/img/maple_leaf.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
   ],
 };
