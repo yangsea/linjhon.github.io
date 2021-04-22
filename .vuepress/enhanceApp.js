251a3bde0c9714ab4fdd69d5d67ddbc8c872bbe3
linjhon <linjhon@foxmail.com>
2021-04-22 09:22:55 +0800

    feat: 移除post路径过滤

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,9 +1,1 @@
-import postMixins from "./mixins/posts";
-
-export default ({ Vue, options, router, siteData, isServer }) => {
-  if (!isServer) {
-    setTimeout(() => {
-      Vue.mixin(postMixins);
-    });
-  }
-};
+export default ({ Vue, options, router, siteData, isServer }) => {};
