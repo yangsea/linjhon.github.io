b7e45fc863826596d043c8eca48656c2d2bf0c06
linjhon <linjhon@foxmail.com>
2021-05-09 18:02:22 +0800

    feat: 增加右侧侧边栏

--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -6,6 +6,7 @@ module.exports = {
   mode: "light", // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
   modePicker: false, // 默认 true，false 不显示模式调节按钮，true 则显示
   locales,
+  subSidebar: "auto",
   sidebar,
   // 搜索设置
   search: true,
